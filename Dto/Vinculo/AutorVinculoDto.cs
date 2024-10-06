da35ef165ac91c6548c3ea07c0cc94be61a8fec2
Luis Felipe <147317513+luis0777@users.noreply.github.com>
2024-10-06 14:25:57 -0300

    Create AutorVinculoDto.cs

---
+++ Dto/Vinculo/AutorVinculoDto.cs
@@ -0,0 +1,9 @@
+namespace ProjetoCrud.Dto.Vinculo
+{
+    public class AutorVinculoDto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+    }
+}
